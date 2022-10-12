e354c5a2ce4fa2e6aea686066e8aed24ca904d15
AlexPaganucci <114147785+AlexPaganucci@users.noreply.github.com>
2022-10-12 11:59:52 +0200

    esercizio w3/d4

---
+++ m1/w3/d3/assets/js/script.js
@@ -0,0 +1,60 @@
+//FUNZIONE SUCCO
+function calcoloSucco (mele, arance) {
+    const succoFrutta = `Succo  con ${mele} mele e ${arance} arance`;
+    return succoFrutta;
+    }
+document.getElementById('corretta').innerHTML = calcoloSucco(4, 5);
+document.getElementById('sbagliata').innerHTML = calcoloSucco(6);
+
+//FUNZIONE CALCOLO ETA
+function calcolaEta(){
+    let annoNascita = 1967;
+    let eta = 2022 - annoNascita;
+    document.getElementById('eta').innerHTML += ` ${eta} anni`;
+}
+calcolaEta();
+
+//FUNZIONE ARROW
+const annoCorrente = 2022;
+var etaAnna = 30;
+var etaMaria = 24;
+
+annoNascitaAnna =() => {
+    return annoCorrente - etaAnna;
+}
+annoNascitaMaria =() => {
+    return annoCorrente - etaMaria;
+}
+document.getElementById('persona1').innerHTML = `L'anno di nascita di Anna è il ${annoNascitaAnna()}`;
+document.getElementById('persona2').innerHTML = `L'anno di nascita di Maria è il ${annoNascitaMaria()}`;
+
+//FUNZIONE DI FUNZIONE
+function ricettaTorta(){
+    const stampaRicetta = (function(){
+        let fetteMela = 9;
+        let fetteArancia = 15;
+        const ricetta = `Torta con ${fetteMela} fette di mela e ${fetteArancia} fette di arancia.`;
+        return ricetta;
+    })();
+
+    return stampaRicetta
+}
+document.getElementById('torta').innerHTML = ricettaTorta();
+
+//form
+document.getElementById('calcola').addEventListener('click', function calcoloSpesa(){
+var cibo = document.getElementById('cibo').value
+var detersivi = document.getElementById('detersivi').value
+var abiti = document.getElementById('abiti').value
+
+parseFloat(cibo);
+parseFloat(detersivi);
+parseFloat(abiti);
+
+const spesa = cibo + detersivi + abiti;
+document.getElementById('totale').innerHTML += spesa;
+})
+
+
+
+
