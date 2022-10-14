298de19cfe60e570a5f9fd5e52e674f8719cda20
AlexPaganucci <114147785+AlexPaganucci@users.noreply.github.com>
2022-10-14 11:49:02 +0200

    progetto/w3

--- m1/w3/progetto/assets/js/script.js
+++ m1/w3/progetto/assets/js/script.js
@@ -10,3 +10,9 @@ function clearAll(){
 function deleteOne(){
     document.getElementById('screen').value = document.getElementById('screen').value.slice(0,-1);
 }
+function radice(){
+    document.getElementById('screen').value = Math.sqrt(document.getElementById('screen').value);
+}
+function quadrato(){
+    document.getElementById('screen').value = Math.pow(document.getElementById('screen').value, 2);
+}
